4946c9e707532dd8d05df41415892e7a3fd8d146
smit_hinsu <smittvhinsu@gmail.com>
2014-02-14 01:09:55 +0530

    lab1 part1

--- lock_server.cc
+++ lock_server.cc
@@ -9,6 +9,16 @@
 lock_server::lock_server():
   nacquire (0)
 {
+  pthread_mutex_init(&globalLock, NULL);
+}
+
+lock_server::~lock_server() 
+{
+  pthread_mutex_destroy(&globalLock);
+  for(typeof(locks.begin()) it = locks.begin(); it != locks.end(); it++)
+  {
+    pthread_mutex_destroy(&it->second);
+  }
 }
 
 lock_protocol::status
@@ -20,4 +30,51 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   return ret;
 }
 
+lock_protocol::status
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  pthread_mutex_t &cur_lock = initLockAndGetRef(lid, 1);
+  pthread_mutex_lock(&cur_lock);
+  
+  lock_protocol::status ret = lock_protocol::OK;
+  return ret;
+}
 
+lock_protocol::status
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  pthread_mutex_t &cur_lock = initLockAndGetRef(lid, 0);
+  pthread_mutex_unlock(&cur_lock);
+  
+  destroyLock(lid);
+  lock_protocol::status ret = lock_protocol::OK;
+  return ret;
+}
+
+pthread_mutex_t&
+lock_server::initLockAndGetRef(lock_protocol::lockid_t lid, int inc) 
+{
+  //pthread_mutex_lock(&globalLock);
+  int cur_count = count[lid];
+  count[lid] += inc;
+  if (cur_count == 0) 
+  {
+    pthread_mutex_init(&locks[lid], NULL);
+    count[lid] = 1;
+  }
+  pthread_mutex_t &ret = locks[lid];
+  //pthread_mutex_unlock(&globalLock);
+  return ret;
+}
+
+void
+lock_server::destroyLock(lock_protocol::lockid_t lid) 
+{
+  //pthread_mutex_lock(&globalLock);
+  int new_count = (--count[lid]);
+  if (new_count == 0) 
+  {
+    pthread_mutex_destroy(&locks[lid]);
+  }
+  //pthread_mutex_unlock(&globalLock);
+}
--- lock_server.h
+++ lock_server.h
@@ -5,19 +5,29 @@
 #define lock_server_h
 
 #include <string>
+#include <map>
+#include <pthread.h>
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
 
 class lock_server {
+  private:
+  pthread_mutex_t& initLockAndGetRef(lock_protocol::lockid_t, int);
+  void destroyLock(lock_protocol::lockid_t);
 
  protected:
   int nacquire;
+  std::map < lock_protocol::lockid_t, pthread_mutex_t > locks;
+  std::map < lock_protocol::lockid_t, unsigned int > count;
+  pthread_mutex_t globalLock;
 
  public:
   lock_server();
-  ~lock_server() {};
+  ~lock_server();
   lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 };
 
 #endif 
